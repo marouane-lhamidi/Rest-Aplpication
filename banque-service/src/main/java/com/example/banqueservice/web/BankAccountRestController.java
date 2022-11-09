65eda9db98d02fc19253f045c4a944f6090e3c3f
Marouane LHAMIDI <lhamidi@mobiblanc.world>
2022-11-09 22:09:14 +0100

    add pictures

--- banque-service/src/main/java/com/example/banqueservice/web/BankAccountRestController.java
+++ banque-service/src/main/java/com/example/banqueservice/web/BankAccountRestController.java
@@ -13,32 +13,25 @@ import java.util.List;
 @AllArgsConstructor
 public class BankAccountRestController {
     IBankAccount iBankAccount;
-
     @GetMapping("bankAccounts")
     public List<BankAccountResponseDto> getAccounts(){
         return iBankAccount.getAccounts();
     }
-
     @GetMapping("bankAccount/{id}")
     public BankAccountResponseDto getAccount(@PathVariable String id){
         return iBankAccount.getAccount(id);
     }
-
     @PostMapping("bankAccount")
     public BankAccountResponseDto addAccount(@RequestBody BankAccountRequestDto bankAccount){
         return iBankAccount.addAccount(bankAccount);
     }
-
     @PutMapping("bankAccount/{id}")
     public BankAccountResponseDto updateAccounts(@PathVariable String id,@RequestBody BankAccountRequestDto bankAccount){
 
         return iBankAccount.updateAccounts(id, bankAccount);
     }
-
     @DeleteMapping("bankAccount/{id}")
     public void delete(@PathVariable String id){
         iBankAccount.delete(id);
     }
-
-
 }
